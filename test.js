094dc79f6f4850eab8f169876b78da05dd7456b6
José Quintana <joseluisquintana20@gmail.com>
2016-01-12 23:55:59 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 var test = require('tape');
 var banr = require('./');
-var spec = '/*! Banr v1.0.0 | MIT (c) 2016 José Luis Quintana */\n';
 
 test('Test Suite', function(t) {
-  t.plan(1);
-  t.equal(banr(), spec, 'Ok when banner string is equal to expected.');
+  t.plan(3);
+  t.ok(banr('./package.json'), 'Ok when path param does exist.');
+  t.notOk(banr('./unknown_package.json'), 'Not ok when path param does not exist.');
+  t.notOk(banr(123), 'Not ok when path param is not valid path.');
 });
